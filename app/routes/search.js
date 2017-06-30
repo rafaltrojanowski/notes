5e48449a3f9c8333d37dd25e0947793fe77f6326
Rafał Trojanowski <rt.trojanowski@gmail.com>
2017-06-30 19:02:30 +0200

    eager return

---
+++ app/routes/search.js
@@ -0,0 +1,48 @@
+import Ember from 'ember'
+
+export default Ember.Route.extend({
+
+  queryParams: {
+    query: {
+      refreshModel: true
+    }
+  },
+
+  beforeModel() {
+    let previousRoutes = this.router.router.currentHandlerInfos;
+    let previousRoute = previousRoutes && previousRoutes.pop();
+
+    if (previousRoute && previousRoute.name != 'search') {
+      localStorage['lastVisitedRoute'] = previousRoute.name
+    }
+  },
+
+  model({ params }) {
+    Ember.run.schedule('afterRender', this, this._fetchNotes);
+  },
+
+  _fetchNotes() {
+    let { controller, store } = this;
+    let { query } = controller;
+
+    controller.set('isLoading', true);
+
+    if (query) {
+      store.query('note', { title: query })
+        .then(this._loadNotes.bind(this))
+        .finally(this._done.bind(this))
+    } else {
+      store.findAll('note')
+        .then(this._loadNotes.bind(this))
+        .finally(this._done.bind(this))
+    }
+  },
+
+  _loadNotes(notes) {
+    this.controller.set('model', notes);
+  },
+
+  _done() {
+    this.controller.set('isLoading', false);
+  }
+});
